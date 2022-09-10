d4698b371db4494d52d9e51de2bc2cfd1efce89a
Muhammad1231 <96456436+Muhammad1231@users.noreply.github.com>
2022-09-09 20:57:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+leftWristY=0;
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialzied");
+}
+ 
+ 
+ song1="music.mp3";
+
+
+
+
+ function preload()
+ {
+    song1=loadSound("music.mp3");
+    
+ }
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = ") + leftWristX +"leftWristY = "+ leftWristY;
+
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX="+rightWristX + "rightWristY = "+rightWristY);
+    }
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+function play()
+ {
+    song1.play();
+ }
